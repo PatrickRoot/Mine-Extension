d8ba0e14a0a03fc8e575cc219f337cbc8d8ff1aa
六楼的雨 <nianqinianyi@163.com>
2017-12-14 17:08:50 +0800

    先改成这样吧，我觉得还是 react 或者 vue 好一点，下一步前端化

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,20 @@
+meFun = {};
+
 $(function () {
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, function (tabs) {
-        let content = $("#tab1-content");
-        let img = $("<div/>");
-        content.append(img);
-        let url = tabs[0].url;
-        new QRCode(img[0], url);
-        content.append($("<div/>").text("标题："+tabs[0].title));
-        content.append($("<div/>").text(JSON.stringify(tabs[0])));
-    });
+    meFun.qrCode = function () {
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, function (tabs) {
+            let content = $("#tab1-content");
+            let img = $("<div/>");
+            content.append(img);
+            let url = tabs[0].url;
+            new QRCode(img[0], url);
+            content.append($("<div/>").text("标题：" + tabs[0].title));
+            content.append($("<div/>").text(JSON.stringify(tabs[0])));
+        });
+    };
 });
 
 chrome.extension.onMessage.addListener(
